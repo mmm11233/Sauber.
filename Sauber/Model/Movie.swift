2a774723db4120305a2b6d3455c65caaa01b5917
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-08-19 10:38:26 +0400

    created movie model

---
+++ Sauber/Model/Movie.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct MoviesResponse: Decodable {
+    let result: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case result = "results"
+    }
+}
+
+struct Movie: Decodable {
+    let id: Int
+    let mediaType: String?
+    let originalName: String?
+    let originalTitle: String?
+    let posterPath: String?
+    let overview: String?
+    let voteCount: Int
+    let releaseDate: String?
+    let voteAverage: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case mediaType = "media_type"
+        case originalName = "original_name"
+        case originalTitle = "original_title"
+        case posterPath = "poster_path"
+        case overview = "overview"
+        case voteCount = "vote_count"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+    }
+}
