d7febe85f475d95b28b4dfeca4b62e7138709fa7
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-08-25 23:37:08 +0400

    modified movie model, created HomeTableViewCellModel and setted the values on the tableView

---
+++ Sauber/Fiatures/Home/HomeTableViewCell/HomeTableViewCellModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeTableViewCellModel.swift
+//  Sauber
+//
+//  Created by Mariam Joglidze on 21/08/2024.
+//
+
+import Foundation
---
+++ Sauber/Fiatures/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Sauber
+//
+//  Created by Mariam Joglidze on 20/08/2024.
+//
+
+import Foundation
