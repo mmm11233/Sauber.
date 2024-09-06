397ed39107c9e9875339b431b1cecc4b8b04dc73
Mariam Joglidze <125551926+mmm11233@users.noreply.github.com>
2024-09-06 23:13:19 +0400

    Update Sauber/Fiatures/ItemList/MoviesListViewController.swift
    Co-authored-by: David T <79376703+davidTens@users.noreply.github.com>

--- Sauber/Fiatures/ItemList/MoviesListViewController.swift
+++ Sauber/Fiatures/ItemList/MoviesListViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class MooviesListViewController: UIViewController {
+final class MoviesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
