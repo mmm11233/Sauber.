d3ef5853df8d2a27bf016f021b654d27ae5c0edf
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-09-11 15:02:48 +0400

    setup movies list view controller

--- Sauber/Fiatures/ItemList /MoviesListViewController.swift
+++ Sauber/Fiatures/ItemList /MoviesListViewController.swift
@@ -1,1 +1,71 @@
 
+import UIKit
+
+final class MoviesListViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupConstraints()
+        setupTableView()
+    }
+    
+    //MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate ([
+            tableView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: 10),
+            tableView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 12),
+            tableView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -12),
+            tableView.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -12)
+        ])
+    }
+    
+         func setupTableView() {
+             tableView.dataSource = self
+             tableView.register(MoviesListTableViewCell.self,
+                                forCellReuseIdentifier: "MoviesListTableViewCell")
+        }
+    }
+    
+    // MARK: - Table View Data Source And Delegate
+    
+extension MoviesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MoviesListTableViewCell", for: indexPath)
+        if let MoviesListTableViewCell = cell as? MoviesListTableViewCell {
+           return MoviesListTableViewCell
+        }
+        
+        return cell
+    }
+}
